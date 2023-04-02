8ac6644ca63b68f453dda7b9b5a67753d1c21e4c
Ardy1an <adithardiyanto1@students.amikom.ac.id>
2023-04-02 10:58:51 +0700

    22.11.5086 tugasP3

---
+++ tugaspemrograman/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tugaspemrograman
+{
+    internal class Program
+    {
+        public string DriverName { get; set; }
+        public bool OnDuty { get; set; }
+        public float NumPassenger { get; set; }
+
+        public void TaxiInfo()
+        {
+            Console.WriteLine("Driver Name : {0}", DriverName);
+            Console.WriteLine("On Duty : {0}", OnDuty);
+            Console.WriteLine("Number of Passenger : {0}", NumPassenger);
+        }
+
+        public void PickUpPassenger()
+        {
+            if (OnDuty == true)
+                Console.WriteLine("{0} sedang menjemput penumpang", DriverName);
+        }
+        public void DropOfPassenger()
+        {
+                Console.WriteLine("{0} selesai mengantar penumpang", DriverName);
+        }
+
+
+          
+    }
+}
---
+++ tugaspemrograman/taxi.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tugaspemrograman
+{
+    internal class TaxiInfo
+    {
+        static void Main(string[] args)
+        {
+            //class sebelumnya "program" harus dipanggil disini agar var bisa terdeteksi
+            Program taxi = new Program();
+
+            taxi.DriverName = "Jono";
+            taxi.OnDuty = true;
+            taxi.NumPassenger = 10;
+
+            taxi.TaxiInfo();
+            taxi.PickUpPassenger();
+            taxi.DropOfPassenger();
+
+            Console.ReadKey();
+                 
+        }
+    }
+}
